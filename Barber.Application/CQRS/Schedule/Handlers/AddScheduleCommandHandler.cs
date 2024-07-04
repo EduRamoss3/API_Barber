f4cfd151367893f585c6f34093642a6969b3a654
Eduardo Ramos <eduardoramossilva@live.com>
2024-07-03 22:14:24 -0300

    This time is already scheduled

--- Barber.Application/CQRS/Schedule/Handlers/AddScheduleCommandHandler.cs
+++ Barber.Application/CQRS/Schedule/Handlers/AddScheduleCommandHandler.cs
@@ -21,6 +21,15 @@ namespace Barber.Application.CQRS.Schedule.Handlers
                 throw new ApplicationException("Error, verify all data before register!");
             }
             Schedules schedules = new Schedules(request.IdBarber, request.IdClient, request.TypeOfService, request.DateSchedule, request.ValueForService,request.IsFinalized);
+            var listSchedules = await _schedulesRepository.GetSchedulesByBarberId(request.IdBarber);    
+
+            foreach(Schedules schedule in listSchedules)
+            {
+                if(schedule.DateSchedule == request.DateSchedule && !schedule.IsFinalized)
+                {
+                    throw new ApplicationException("This time is already scheduled");
+                }
+            }
             await _schedulesRepository.AddNewSchedule(schedules);
             return schedules;
         }
