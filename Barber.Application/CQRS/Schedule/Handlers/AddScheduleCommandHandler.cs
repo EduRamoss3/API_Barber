b2ed3638e8ef2626ffd06accbe36207372b59c95
Eduardo Ramos <eduardoramossilva@live.com>
2024-07-04 07:21:48 -0300

    Update

--- Barber.Application/CQRS/Schedule/Handlers/AddScheduleCommandHandler.cs
+++ Barber.Application/CQRS/Schedule/Handlers/AddScheduleCommandHandler.cs
@@ -25,7 +25,7 @@ namespace Barber.Application.CQRS.Schedule.Handlers
 
             foreach (Schedules schedule in listSchedules)
             {
-                if (request.DateSchedule == request.DateSchedule && !request.IsFinalized)
+                if (request.DateSchedule == schedule.DateSchedule && !request.IsFinalized)
                 {
                     throw new DomainExceptionValidation("This time is already scheduled");
                 }
